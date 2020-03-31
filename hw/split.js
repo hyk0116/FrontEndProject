c1cb7590f315c0fd3d6e5e678be3deeee79499f0
Hwang hyunki <hyunki8389@gmail.com>
2020-03-31 16:40:30 +0900

    문자열자르기

---
+++ hw/split.js
@@ -0,0 +1,7 @@
+let text="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."; 
+
+let arr=text.split(" ")
+
+for(let i=0; i<arr.length; i++){
+    console.log("["+arr[i]+"]")
+}
