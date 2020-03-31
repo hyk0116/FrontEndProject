01826fd0181049c997fdeec90ee585ad3b2b2284
Hwang hyunki <hyunki8389@gmail.com>
2020-03-31 16:39:00 +0900

    짝수제거하기

---
+++ hw/even.js
@@ -0,0 +1,16 @@
+let a=[]
+
+for(let i=0; i<100; i++){
+    a[i]=Math.floor(Math.random()*100+1)
+}
+
+for(let i=0; i<a.length; i++){
+    if(a[i]%2==0)
+    a.splice(i-1,1)
+}
+
+
+console.log("["+a.toString()+"]")
+
+
+
