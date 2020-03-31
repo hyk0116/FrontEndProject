2ec58fe10d45df305a56397d7f0475a744a8a8a9
Hwang hyunki <hyunki8389@gmail.com>
2020-03-31 17:06:03 +0900

    모음위치찾기 (수정)

--- hw/vowel.js
+++ hw/vowel.js
@@ -1,8 +1,10 @@
 let text="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."; 
 
-
+let count=0//모음 카운트
 
 for(let i=0; i<text.length; i++){
-    if(text[i].search(/[aeiouAEIOU]/)==0)
-    console.log(i)
+    if(text[i].search(/[^aeiou]/i)){//정규식을 이용하여 모음찾기
+    count++;
+    console.log(count+"번째 모음 "+text[i]+"는 "+i+"번째위치");
+    }
 }
