366e3ca4a3be922490e6cf98513ca792e10f65ce
Hwang hyunki <hyunki8389@gmail.com>
2020-04-07 19:31:31 +0900

    3주차 과제

---
+++ hw/hw3/space.js
@@ -0,0 +1,4 @@
+let text="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."; 
+
+
+console.log(text.replace(/\s/g,""))
---
+++ hw/hw3/split.js
@@ -0,0 +1,5 @@
+let text="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."; 
+
+let arr=text.split(" ")
+
+console.log(arr)
