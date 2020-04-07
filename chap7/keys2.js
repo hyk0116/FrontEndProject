4558fe9a4584c8aada13423800868fedaea809e6
Hwang hyunki <hyunki8389@gmail.com>
2020-04-07 19:15:29 +0900

    4주차 강의

---
+++ chap7/keys2.js
@@ -0,0 +1,2 @@
+let person ={name: "홍길동",age: 16, depratment:"소프"}
+console.log(Object.values(person))
---
+++ chap7/person4.js
@@ -0,0 +1,9 @@
+function creatPerson(s,i){
+    return {name:s ,age: i}
+}
+
+let person1 =creatPerson("홍길동",16)
+let person2 =creatPerson("임꺽정",18)
+
+console.log(person1)
+console.log(person2)
